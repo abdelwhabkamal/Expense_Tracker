3ae5bf8d9396a464008c2d6cfb358a00e0e3ca08
abdelwhabkamal <abdewhabkamal17@gmail.com>
2024-06-19 04:14:39 +0300

    [database_connection]: connect with postgresql

--- expensetracker/expensetracker/settings.py
+++ expensetracker/expensetracker/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'expensetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'incomeexpensesdb',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD':'admin'
     }
 }
 
