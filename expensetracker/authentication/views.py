8fd647549c0041cd02a60b514340062c8dc539ff
abdelwhabkamal <abdewhabkamal17@gmail.com>
2024-06-20 20:14:48 +0300

    [email]: email validation

--- expensetracker/authentication/views.py
+++ expensetracker/authentication/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
+
 # Create your views here.
 
 class UsernameValidationView(View):
@@ -14,6 +16,16 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
         return JsonResponse({'username_valid': True})
+    
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
+        return JsonResponse({'email_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
